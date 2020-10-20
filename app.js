61ac847d9de842b702e5dc529468b7ea5e947c16
Ashutosh Kumar <ashu.heretocode@gmail.com>
2020-10-21 02:06:22 +0530

    created homepage

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const mongoose = require('mongoose');
 //connect to database locally
 
 mongoose.connect('mongodb://localhost/blogSite',{useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>app.listen(3000,()=>console.log('connected to database...')))
+.then(()=>app.listen(4000,()=>console.log('connected to database...')))
 .catch(err=>console.log(err));
+
+//middlewares
+app.set('view engine','ejs');
+app.use(express.static('public'));
 
 //testing the app
 
 app.get('/',(req,res)=>{
-    res.send('We are at homepage');
+    res.render('home',{title:'Home'});
 })
 
-//404 page
+
+// 404 page
 app.use((req,res)=>res.status(404).send('404 page not found'));
